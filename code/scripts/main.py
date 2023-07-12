365dd110b7119191b2770985ceff016f6b5c59ef
J. A. Heuer <j.a.heuer@outlook.com>
2023-07-12 12:31:02 +0200

    train function first function (still buggy: nan after some epochs)

--- code/scripts/main.py
+++ code/scripts/main.py
@@ -1,32 +1,47 @@
 import numpy as np 
 import torch 
+from torch.utils.data import TensorDataset , DataLoader
+from torch.optim import Adam
 
 from dataset import DatasetKDD
 from model import VAE_Tabular
-from torch.utils.data import TensorDataset 
-
+from train import train_vae_tabular
 
 def main():
-    print("Hello")
+
+    # HYPERPARAMETERS
+    LEARNING_RATE = 0.0005
+    BATCH_SIZE = 256
+    NUM_EPOCHS = 50
+
+    # Device
+    CUDA_DEVICE_NUM = 1
+    DEVICE = torch.device(f'cuda:{CUDA_DEVICE_NUM}' if torch.cuda.is_available() else 'cpu')
+    print('Device:', DEVICE)
+
 
     # LOAD DATA (full; no split)
-    data = DatasetKDD(is_debug=True)
+    data:DatasetKDD = DatasetKDD(is_debug=True)
     dataset:TensorDataset = data.get_data()
-    
-    # select one row
-    x1, y1 = dataset[0]
+    loader_train:DataLoader = DataLoader(dataset, batch_size=16, shuffle=True)
 
     # MODEL
-    model = VAE_Tabular()
-    encoded, z_mean, z_log_var, decoded  = model.forward(x1)
+    model:VAE_Tabular = VAE_Tabular()
+    model.to(DEVICE)
 
-    print(x1)
-    print(encoded)
-    print(z_mean)
-    print(z_log_var)
-    print(decoded)
+    # OPTIMIZER
+    optimizer:Adam = Adam(model.parameters(), lr=LEARNING_RATE)
 
-    #a = model.forward()
+    # TRAINING
+    log_dict:dict = train_vae_tabular(
+        model=model, 
+        num_epochs=10 ,
+        optimizer=optimizer, 
+        device=DEVICE, 
+        train_loader=loader_train,
+        skip_epoch_stats=True,
+        logging_interval=1500
+    )
 
 if __name__ == "__main__": 
     main()
